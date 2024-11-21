5d0c4dd5094eb1ad7069ecf6e0083061dccde2e7
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-11-21 21:06:23 +0700

    menambahkan function FormulirView

--- app/src/main/java/com/example/meet7/ui/view/FormulirView.kt
+++ app/src/main/java/com/example/meet7/ui/view/FormulirView.kt
@@ -1,3 +1,9 @@
 package com.example.meet7.ui.view
 
+import androidx.compose.ui.Modifier
 
+fun FormulirView(
+    modifier: Modifier = Modifier,
+    listJK: List<String>,
+    onSubmitClicked: (MutableList<String>) -> Unit
+){}
