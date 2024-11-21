1f3b4d77c1d316d303dd476da074df1be2b4561d
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-11-21 21:40:53 +0700

    menambahkan function untuk menampilkan data setelah data di submit

--- app/src/main/java/com/example/meet7/ui/view/TampilDataView.kt
+++ app/src/main/java/com/example/meet7/ui/view/TampilDataView.kt
@@ -1,4 +1,52 @@
 package com.example.meet7.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.meet7.model.DataSiswa
 
+@Composable
+fun TampilDataView(uiState: DataSiswa, onBackButton : () -> Unit){
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp))
+    {
+        TampilData(x = "Nama", y = uiState.nama)
+        TampilData(x = "Jenis Kelamin", y = uiState.gender)
+        TampilData(x = "Alamat", y = uiState.alamat)
+        TampilData(x = "Email", y = uiState.email)
+        TampilData(x = "NoTelepon", y = uiState.notelepon)
+        TampilData(x = "NIM", y = uiState.nim)
+        Button(onClick = onBackButton) {
+            Text(text = "kembali")
+        }
+    }
+}
+
+@Composable
+fun TampilData(x : String, y :String){
+    Column (
+        modifier = Modifier.padding(16.dp)
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween)
+        {
+            Text(text = x,
+                modifier = Modifier.weight(0.8f))
+            Text(text = ":",
+                modifier = Modifier.weight(0.2f))
+            Text(text = y,
+                modifier = Modifier.weight(2f))
+        }
+    }
+}
 
