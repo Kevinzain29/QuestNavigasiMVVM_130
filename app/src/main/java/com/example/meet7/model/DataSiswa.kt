a199b415637fa4bafa18ff6d74705c0556561e72
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-11-21 21:00:17 +0700

    menambahkan Class yang didalamnya terdapat variable untuk menyimpan data

--- app/src/main/java/com/example/meet7/model/DataSiswa.kt
+++ app/src/main/java/com/example/meet7/model/DataSiswa.kt
@@ -1,3 +1,10 @@
 package com.example.meet7.model
 
-
+data class DataSiswa(
+    val nama: String = "",
+    val alamat: String = "",
+    val gender: String = "",
+    val email: String = "",
+    val notelepon: String= "",
+    val nim: String= ""
+)
