3bb49a2e4c05772d248420d574ef009800c39053
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-11-21 21:25:18 +0700

    membuat Column dan TextField untuk input data nama

--- app/src/main/java/com/example/meet7/ui/view/FormulirView.kt
+++ app/src/main/java/com/example/meet7/ui/view/FormulirView.kt
@@ -1,11 +1,21 @@
 package com.example.meet7.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormulirView(
@@ -22,4 +32,30 @@ fun FormulirView(
 
     val listData: MutableList<String> = mutableListOf(nama, gender, alamat, email, notelepon, nim)
 
+    Column (
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        TextField(
+        value = nama,
+        onValueChange = {nama = it},
+        label = {
+            Text(text = "nama")
+        },
+        placeholder = {
+            Text(text = "isi nama anda")
+        },
+        modifier = modifier.fillMaxWidth().padding(5.dp)
+    )
+        Row (modifier = Modifier.fillMaxWidth()){
+            listJK.forEach{selectedGender->
+                Row (verticalAlignment = Alignment.CenterVertically){
+                    RadioButton(
+                        selected = gender == selectedGender,
+                        onClick = {gender = selectedGender}
+                    )
+                    Text(text = selectedGender)}
+            }
+        }
+    }
 }
