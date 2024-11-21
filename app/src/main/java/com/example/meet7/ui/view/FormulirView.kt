c4d5d52864d0e68257176c23d98e0572b3b945f0
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-11-21 21:13:45 +0700

    membuat variable untuk menyimpan input data sementara dan menambahkan listData

--- app/src/main/java/com/example/meet7/ui/view/FormulirView.kt
+++ app/src/main/java/com/example/meet7/ui/view/FormulirView.kt
@@ -1,9 +1,25 @@
 package com.example.meet7.ui.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
+@Composable
 fun FormulirView(
     modifier: Modifier = Modifier,
     listJK: List<String>,
     onSubmitClicked: (MutableList<String>) -> Unit
-){}
+){
+    var nama by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var notelepon by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
+    var nim by remember { mutableStateOf("") }
+
+    val listData: MutableList<String> = mutableListOf(nama, gender, alamat, email, notelepon, nim)
+
+}
